498e5916246abb1163117ff1805027849b2ebfaa
Estherayeola <estheraiyeola@yahoo.com>
2024-09-02 11:50:20 +0100

    First task 6 out of 9 tests passing

--- src/main/java/org/example/BetweenTwoSets.java
+++ src/main/java/org/example/BetweenTwoSets.java
@@ -1,4 +1,60 @@
 package org.example;
 
+import java.util.*;
+
 public class BetweenTwoSets {
+
+    public int findNumbers(List<Integer> a, List<Integer> b) {
+        List<Integer> numbersBetween = new ArrayList<>();
+        List<Integer> numbersNeeded = new ArrayList<>();
+        int lengthOfArray1 = a.size();
+        int lengthOfArray2 = b.size();
+        Collections.sort(a);
+        Collections.sort(b);
+        int rangeStart = (a.size() >= 2) ? a.get(1) : a.get(0) + 1;
+        int rangeEnd = b.get(0);
+
+        for (int i = rangeStart; i <= rangeEnd; i++) {
+            numbersBetween.add(i);
+        }
+
+
+
+        for (int number : numbersBetween) {
+            boolean isValid = true;
+            for (int a1 = 0; a1 < lengthOfArray1; a1++) {
+                if (number % a.get(a1) != 0){
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid){
+                for (int a2 = 0; a2 < lengthOfArray2; a2++) {
+                    if (b.get(a2) % number != 0){
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+            if (isValid){
+                numbersNeeded.add(number);
+            }
+        }
+
+
+        System.out.println(numbersBetween);
+        System.out.println(numbersNeeded);
+        return numbersNeeded.size();
+    }
+    //        for (int number = 0; number < numbersBetween.size(); number++) {
+//            for (int a1 = 0; a1 < lengthOfArray1; a1++) {
+//                for (int a2 = 0; a2 < lengthOfArray2; a2++) {
+//                    counter++;
+//                    if (numbersBetween.get(number) % array1[a1] == 0 && array2[a2] % numbersBetween.get(number) == 0 && counter == lengthOfArray2){
+//                        numbersNeeded.add(numbersBetween.get(number));
+//                    }
+//                }
+//                counter = 0;
+//            }
+//        }
 }
--- src/test/java/org/example/BetweenTwoSetsTest.java
+++ src/test/java/org/example/BetweenTwoSetsTest.java
@@ -1,6 +1,89 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BetweenTwoSetsTest {
-  
+
+    @Test
+    public void testThatTheNumbersBetweenTheTwoArraysCanBeGotten(){
+        BetweenTwoSets betweenTwoSets = new BetweenTwoSets();
+        List<Integer> array1 = new ArrayList<>();
+        array1.add(2);
+        array1.add(6);
+        List<Integer> array2 = new ArrayList<>();
+        array2.add(24);
+        array2.add(36);
+
+
+       int numbers = betweenTwoSets.findNumbers(array1, array2);
+
+
+        assertEquals(2, numbers);
+
+
+    }
+
+    @Test
+    public void testThatTheNumbersBetweenTheTwoArraysCanBeGotten2(){
+        BetweenTwoSets betweenTwoSets = new BetweenTwoSets();
+        List<Integer> array1 = new ArrayList<>();
+        array1.add(2);
+        array1.add(4);
+        List<Integer> array2 = new ArrayList<>();
+        array2.add(16);
+        array2.add(32);
+        array2.add(96);
+
+
+        int numbers = betweenTwoSets.findNumbers(array1, array2);
+
+        assertEquals(3, numbers);
+
+
+    }
+
+    @Test
+    public void testThatTheNumbersBetweenTheTwoArraysCanBeGotten3(){
+        BetweenTwoSets betweenTwoSets = new BetweenTwoSets();
+        List<Integer> array1 = new ArrayList<>();
+        array1.add(2);
+
+        List<Integer> array2 = new ArrayList<>();
+        array2.add(20);
+        array2.add(30);
+        array2.add(10);
+
+
+        int numbers = betweenTwoSets.findNumbers(array1, array2);
+
+        assertEquals(1, numbers);
+
+
+    }
+
+    @Test
+    public void testThatTheNumbersBetweenTheTwoArraysCanBeGotten4(){
+        BetweenTwoSets betweenTwoSets = new BetweenTwoSets();
+        List<Integer> array1 = new ArrayList<>();
+        array1.add(1);
+
+        List<Integer> array2 = new ArrayList<>();
+        array2.add(72);
+        array2.add(48);
+
+
+
+        int numbers = betweenTwoSets.findNumbers(array1, array2);
+
+        assertEquals(8, numbers);
+
+
+    }
 }
